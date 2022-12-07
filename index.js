4c6dc68a86ed6b750d6ef60cacef155abe4f64f5
David Viitanen <david.viitanen@gmail.com>
2022-12-07 03:13:54 +0100

    Test push.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
+const colorSwitch = document.getElementById('input-color-switch');
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-})
+colorSwitch.addEventListener('click', checkMode);
+
+function checkMode(){
+    console.log('checking...');
+    if(colorSwitch.checked){
+        console.log('dark on');
+        darkModeOn();
+    }
+    else{
+        console.log('dark off');
+        darkModeOff();
+    }
+}
+
+function darkModeOn(){
+    document.body.classList.add("dark-mode");
+}
+
+function darkModeOff(){
+    document.body.classList.remove("dark-mode");
+}
